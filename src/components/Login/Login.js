be299ace6d62fbdb4aeb9c9ab8aea16757a20b6f
yhagiwara346 <yhagiwara@fordham.edu>
2021-10-24 15:09:51 -0400

    add option to login and signup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,30 @@
-import React from "react";
+import {React, useState} from "react";
 import TextField from '@mui/material/TextField';
 import { Link } from "react-router-dom";
 
 const Login = () => {
+    const [loginType, setLoginType] = useState("recruiter");
+
+    const changeType = (e) => {
+        if(e.target.name === "recruiter"){
+            setLoginType("recruiter");
+        }else{
+            setLoginType("candidate");
+        }
+    }
 
     return (<div className="login-page">
     <div className="form-container-login">
         <div className="form">
         <h1 className="login-title">Login</h1>
+            <div className="type-container">
+                <button className={loginType === "recruiter" ? "type-container-btn-toggle": "type-container-btn"} 
+                        name="recruiter" 
+                        onClick={changeType}>I'm a Recruiter</button>
+                <button className={loginType === "candidate" ? "type-container-btn-toggle": "type-container-btn"} 
+                        name="candidate" 
+                        onClick={changeType}>I'm a Job Seeker</button>
+            </div>
             <div className="form-container-input">
                 <TextField
                     name = "username" 
@@ -31,7 +48,8 @@ const Login = () => {
             </div>
             <button className="login-btn" >Login</button>
             <p style={{margin:"18px 0 10px 0", color:"#F32013"}}></p>
-            <p style={{margin:"20px 0 10px 0"}}>Do you have an account yet?  <Link style={{textDecoration:"none"}} to="signup">Signup</Link></p>
+            <p style={{margin:"20px 0 10px 0"}}>Do you have an account yet?  <Link style={{textDecoration:"none"}} 
+                                                                                   to="signup">Signup</Link></p>
         </div>
     </div>
     </div>) 
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,13 +1,31 @@
-import React from "react";
+import {React, useState} from "react";
 import TextField from '@mui/material/TextField';
 import { Link  } from "react-router-dom"; //for routing
 
 const Signup = () => {
 
+    const [signupType, setSignupType] = useState("recruiter");
+
+    const changeType = (e) => {
+        if(e.target.name === "recruiter"){
+            setSignupType("recruiter");
+        }else{
+            setSignupType("candidate");
+        }
+    }
+
     return (<div className="signup-page">
             <div className="form-container-signup">
                 <form className="form" >
-                <h1 className="signup-title">Sign up</h1>
+                    <h1 className="signup-title">Sign up</h1>
+                    <div className="type-container">
+                        <button className={signupType === "recruiter" ? "type-container-btn-toggle": "type-container-btn"} 
+                                name="recruiter" 
+                                onClick={changeType}>I'm a Recruiter</button>
+                        <button className={signupType === "candidate" ? "type-container-btn-toggle": "type-container-btn"} 
+                                name="candidate" 
+                                onClick={changeType}>I'm a Job Seeker</button>
+                    </div>
                     <div className="form-container-input">
                         <TextField
                             name = "username" 
@@ -41,7 +59,8 @@ const Signup = () => {
                     </div>
                     <button className="signup-btn">Sign up</button>
                     <p style={{margin:"15px 0 15px 0", color:"#F32013"}}></p>
-                    <p style={{margin:"0 0 30px 0"}}>Already have an account?  <Link style={{textDecoration:"none"}} to="login">Login</Link></p>
+                    <p style={{margin:"0 0 30px 0"}}>Already have an account? <Link style={{textDecoration:"none"}} 
+                                                                                    to="login">Login</Link></p>
                 </form>
             </div>
             </div>) 
